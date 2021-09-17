f42073ed20966ab00ae105369f3863e6d3c59a65
Sebastian Cordero <scordero414@gmail.com>
2021-09-17 18:17:26 -0500

    First project in .NET

---
+++ Shared/Entity/Movie.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proy1.Shared.Entity
+{
+    public class Movie
+    {
+        public string Name{get;set;}
+        public string Sinopsis{get;set;}
+        public DateTime Premier{get;set;}
+    }
+}
